7b1fcbb7d458b8985337a4a6c86b97af84285dbe
angelrosmend <angelrosmend@gmail.com>
2020-09-09 14:15:45 -0300

    first commit

---
+++ src/layout/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import '../css/styles.css'
+import Nav from './Nav'
+
+
+function Home() {
+    return (
+        <div className="home">
+            <Nav/>
+            <div className="form"></div>
+        </div>
+    )
+}
+
+export default Home
