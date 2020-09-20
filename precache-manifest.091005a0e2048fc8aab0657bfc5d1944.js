4589d0e564c8921dba10e1f767f148d473e12440
angelrosmend <angelrosmend@gmail.com>
2020-09-20 20:40:28 -0300

    Updates

---
+++ precache-manifest.091005a0e2048fc8aab0657bfc5d1944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b76342264efd165cde882c80689f7fe",
+    "url": "/CV-react/index.html"
+  },
+  {
+    "revision": "db5a89d04c1a0d34d242",
+    "url": "/CV-react/static/css/main.e5db312b.chunk.css"
+  },
+  {
+    "revision": "2fb72bb529d809da7ee6",
+    "url": "/CV-react/static/js/2.06ae48ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-react/static/js/2.06ae48ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5a89d04c1a0d34d242",
+    "url": "/CV-react/static/js/main.e01594a8.chunk.js"
+  },
+  {
+    "revision": "f14e7d624b9008f39c34",
+    "url": "/CV-react/static/js/runtime-main.746a6db6.js"
+  },
+  {
+    "revision": "5a1956e5fc1aad83f4b6b91de609b4ef",
+    "url": "/CV-react/static/media/photo.5a1956e5.jpeg"
+  }
+]);
