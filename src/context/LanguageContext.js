7784ad47e67b2e8dfeecb4c3238cfd73e28ef979
angelrosmend <angelrosmend@gmail.com>
2020-10-11 21:33:00 -0300

    fixing button bg

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -7,7 +7,6 @@ export const LanguageContext = createContext()
 export const LanguageProvider = (props) => {
     const [language, setLanguage] = useState(es)
     const [clicked, setClicked] = useState(false)
-    const localData = localStorage.getItem('lang');
         
     const selectEsp = () => {
         setLanguage(es)
@@ -18,10 +17,6 @@ export const LanguageProvider = (props) => {
         setLanguage(en)
         setClicked(true)
     }
-
-    useEffect(() => {
-        window.localStorage.setItem('lang', JSON.stringify(language))
-    }, [language])
 
     return(
         <LanguageContext.Provider value={{language,clicked, selectEsp, selectEng}}>
